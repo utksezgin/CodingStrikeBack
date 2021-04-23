53a8f20e0434b672be47e6524a9276deb90892a5
Utku Sezgin <utksezgin@gmail.com>
2021-04-23 19:45:44 +0300

    Wood League Boss 2

--- CodingStrikeBack/Source.cpp
+++ CodingStrikeBack/Source.cpp
@@ -6,9 +6,8 @@
 using namespace std;
 
 /**
- * This code automatically collects game data in an infinite loop.
- * It uses the standard input to place data into the game variables such as x and y.
- * YOU DO NOT NEED TO MODIFY THE INITIALIZATION OF THE GAME VARIABLES.
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
  **/
 
 int main()
@@ -17,21 +16,40 @@ int main()
     // game loop
     while (1)
     {
-        int x; // x position of your pod
-        int y; // y position of your pod
+        int x;
+        int y;
         int nextCheckpointX; // x position of the next check point
         int nextCheckpointY; // y position of the next check point
-        cin >> x >> y >> nextCheckpointX >> nextCheckpointY; cin.ignore();
+        int nextCheckpointDist; // distance to the next checkpoint
+        int nextCheckpointAngle; // angle between your pod orientation and the direction of the next checkpoint
+        cin >> x >> y >> nextCheckpointX >> nextCheckpointY >> nextCheckpointDist >> nextCheckpointAngle; cin.ignore();
+        int opponentX;
+        int opponentY;
+        cin >> opponentX >> opponentY; cin.ignore();
 
         // Write an action using cout. DON'T FORGET THE "<< endl"
         // To debug: cerr << "Debug messages..." << endl;
 
+        int thrust = 100;
+        if (nextCheckpointAngle < -90 || nextCheckpointAngle > 90)
+        {
+            thrust = 0;
+        }
+        else
+        {
+            if (nextCheckpointDist < 50)
+            {
+                thrust = 35;
+            }
+            else
+            {
+                thrust = 100;
+            }
+        }
 
-        // Edit this line to output the target position
-        // and thrust (0 <= thrust <= 100)
+        // You have to output the target position
+        // followed by the power (0 <= thrust <= 100)
         // i.e.: "x y thrust"
-        cout << nextCheckpointX << " " << nextCheckpointY << " 70" << endl;
-
-
+        cout << nextCheckpointX << " " << nextCheckpointY << " " << thrust << endl;
     }
 }
